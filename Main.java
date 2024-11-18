4852c2aaedb91fa4c6f7c47c60a9cf5ff7aaf698
Dhanush <dhanushdhanu00000@gmail.com>
2024-11-18 21:33:26 +0530

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-package com.practise3;
-
-public class Main
-{
-    public static void main(String [] args){
-        System.out.println("Testing...");
-        MyWindow w = new MyWindow();
-    }
-}
