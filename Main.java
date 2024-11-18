0815925975c8ff40729b16effd577063eb03d667
Dhanush <dhanushdhanu00000@gmail.com>
2024-11-18 21:31:48 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+package com.practise3;
+
+public class Main
+{
+    public static void main(String [] args){
+        System.out.println("Testing...");
+        MyWindow w = new MyWindow();
+    }
+}
